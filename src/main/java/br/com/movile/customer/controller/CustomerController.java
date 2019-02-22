790ac30727f4f29e86d776771c2687ae8bcea754
Taila Saviani <tailanayara@gmail.com>
2019-02-21 23:54:25 -0300

    Criacao dos metodos getListCustomers e getCustomer

--- src/main/java/br/com/movile/customer/controller/CustomerController.java
+++ src/main/java/br/com/movile/customer/controller/CustomerController.java
@@ -1,11 +1,13 @@
 package br.com.movile.customer.controller;
 
+import br.com.movile.customer.model.Customer;
 import br.com.movile.customer.service.CustomerService;
-import br.com.movile.exception.model.NoMotoboyAvailableException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("customers")
@@ -13,5 +15,30 @@ public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+
+    @GetMapping
+    public List<Customer> getListCustomers(){
+        return customerService.getListCustomers();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Customer> getCustomer(@PathVariable("id") String id) throws Exception {
+        return customerService.getCustomer(id);
+    }
+
+    //TODO: Exception de Customer jah existente
+    //TODO: Verificar cliente existente sem contar o ID.
+    //TODO: Ao criar Cliente id automatico.
+    @PostMapping()
+    public ResponseEntity<Customer> insertCustomer(@RequestBody Customer customer) throws Exception {
+
+        if(customerService.getCustomer(customer.getId()).isPresent()){
+
+            return ResponseEntity.badRequest().build();
+        }else{
+            Customer newCustomer = customerService.insetCustomer(customer);
+            return ResponseEntity.ok(newCustomer);
+        }
+    }
 
 }
--- src/main/java/br/com/movile/customer/service/CustomerService.java
+++ src/main/java/br/com/movile/customer/service/CustomerService.java
@@ -1,12 +1,38 @@
 package br.com.movile.customer.service;
 
+import br.com.movile.customer.model.Customer;
 import br.com.movile.customer.repository.CustomerRepository;
+import br.com.movile.exception.handler.MapFoodExceptionHandler;
+import br.com.movile.exception.model.dto.MapFoodException;
+import ch.qos.logback.core.net.server.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    //TODO: Fazer Paginacao
+    public List<Customer> getListCustomers(){
+
+        return customerRepository.findAll();
+    }
+    //TODO: Lancar exception correta - Customer not Found (especifica)
+    public Optional<Customer>  getCustomer(String id) throws Exception {
+        Optional<Customer> customer =  customerRepository.findById(id);
+        if (!customer.isPresent())
+            throw new Exception("Customer not Found");
+        return customer;
+    }
+
+    public Customer insetCustomer(Customer customer){
+        return customerRepository.insert(customer);
+    }
+
+
 }
