7f8ea3af93bb164df2e933f3f2bf8f2b462abe3b
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-02-26 10:59:29 -0300

    inserindo exception para validação de restaurant no post do order

--- src/main/java/br/com/movile/order/service/OrderService.java
+++ src/main/java/br/com/movile/order/service/OrderService.java
@@ -2,18 +2,31 @@ package br.com.movile.order.service;
 
 import br.com.movile.order.model.Order;
 import br.com.movile.order.repository.OrderRepository;
+import br.com.movile.restaurant.repository.RestaurantRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    public OrderService(OrderRepository orderRepository) {
+    private final RestaurantRepository restaurantRepository;
+
+    public OrderService(OrderRepository orderRepository, RestaurantRepository restaurantRepository) {
         this.orderRepository = orderRepository;
+        this.restaurantRepository = restaurantRepository;
     }
 
     public Order save(Order order) {
+        Optional restaurant = restaurantRepository.findById(order.getRestaurant().getId());
+
+        if (!restaurant.isPresent() || order.getRestaurant().getId().equals("null")) {
+            throw new NoSuchElementException("Restaurante inválido e/ou não encontrado!");
+        }
+
         return orderRepository.save(order);
     }
 }
