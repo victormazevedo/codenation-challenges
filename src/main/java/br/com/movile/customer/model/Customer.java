84015220579d52b19381f7e2ae7aa8228e0cf4d8
Taila Nayara Saviani <taila.saviani@ibm.com>
2019-03-08 08:57:59 -0300

    Insercao do Lombok

--- src/main/java/br/com/movile/customer/model/Customer.java
+++ src/main/java/br/com/movile/customer/model/Customer.java
@@ -3,11 +3,11 @@ package br.com.movile.customer.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Getter;
+import lombok.Data;
 
 
-@Getter
 @Document(collection = "customer")
+@Data
 public class Customer {
 
 	@Id
@@ -15,30 +15,11 @@ public class Customer {
 	private double longitude;
 	private double latitude;
 
-	public String getId() {
-		return id;
-	}
-
-	public double getLongitude() {
-		return longitude;
-	}
-
-	public double getLatitude() {
-		return latitude;
-	}
-
 	public Customer(String id, double longitude, double latitude) {
+		super();
 		this.id = id;
 		this.longitude = longitude;
 		this.latitude = latitude;
 	}
 
-	@Override
-	public String toString() {
-		return "Customer{" +
-				"id='" + id + '\'' +
-				", longitude=" + longitude +
-				", latitude=" + latitude +
-				'}';
-	}
 }
